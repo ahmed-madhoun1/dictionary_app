1d6793c5e82212d2157f3171fb2f6aac26d038e1
ahmed-madhoun1 <ahmed2madhoun2@gmail.com>
2025-04-21 18:54:19 +0300

    feature/added localization

--- lib/core/utils/Constants.dart
+++ lib/core/utils/Constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://20.100.3.74:8000/';
+  static const String baseUrl = 'URL';
   static const String getWords = 'api/v1/words/search/';
   static const String getWordsQueryParam = 'q';
 }
